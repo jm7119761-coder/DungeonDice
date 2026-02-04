d9c7ea422ebafae1da6335d27020df19b3a1a638
jm7119761-coder <jm7119761@gmail.com>
2026-02-04 14:12:28 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by MORALES, JACK on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
